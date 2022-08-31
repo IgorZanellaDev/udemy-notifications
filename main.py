7ae4af87f97c10668166ece483469687c54e0414
Igor Zanella <igorzanella.dev@gmail.com>
2022-09-01 01:58:56 +0200

    feat: add telegram sending message and checks

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from pprint import pprint
 import requests
 from dotenv import load_dotenv
 import os
+import time
+
+
+def send_msg(text):
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    print(f"Sending message: {text}")
+    url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
+    requests.get(url_req, timeout=10)
+
+
+def get_enrollers():
+    enrollers_client = requests.get(
+        "https://www.udemy.com/api-2.0/instructor-performance/enrollment-metrics/?fields[user]=total,current_month,daily,monthly",
+        cookies=cookies, timeout=10)
+    return enrollers_client.json()["results"][0]["total"]
+
+def get_earned():
+    earned_client = requests.get(
+        "https://www.udemy.com/api-2.0/share-holders/v1.0/371956/total/?timeframe=month&aggregate=day&breakdown=revenue_type",
+        cookies=cookies, timeout=10)
+    return earned_client.json()["amount"]["amount"]
+
 
 load_dotenv()
 
@@ -9,9 +31,20 @@ cookies = {
     "access_token": os.getenv("ACCESS_TOKEN"),
 }
 
-enrollers = requests.get("https://www.udemy.com/api-2.0/instructor-performance/enrollment-metrics/?fields[user]=total,current_month,daily,monthly", cookies=cookies)
+starttime = time.time()
 
-total = requests.get("https://www.udemy.com/api-2.0/share-holders/v1.0/371956/total/?timeframe=month&aggregate=day&breakdown=revenue_type", cookies=cookies)
+actual_enrollers = 0
+actual_earned = 0
 
-pprint(enrollers.text)
-pprint(total.text)
+while True:
+    enrollers = get_enrollers()
+    if actual_enrollers != enrollers:
+        actual_enrollers = enrollers
+        send_msg(f"🙋 Nuovo studente, ora: {enrollers} studenti")
+
+    earned = get_earned()
+    if actual_earned != earned:
+        actual_earned = earned
+        send_msg(f"🤑 Nuove entrate, ora: {earned}$")
+
+    time.sleep(60.0 - ((time.time() - starttime) % 60.0))
